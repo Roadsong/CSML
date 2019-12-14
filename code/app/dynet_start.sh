22d807140c2d29d277ca54ce854c9d182c1a89c7
Roadsong <echo-inception@live.com>
2019-12-14 17:56:34 -0600

    Update dynet_start.sh

--- code/app/dynet_start.sh
+++ code/app/dynet_start.sh
@@ -1,4 +1,4 @@
 wget -P /app_code https://raw.githubusercontent.com/Roadsong/CSML/master/code/app/config.py
-wget -P /app_code https://raw.githubusercontent.com/Roadsong/CSML/master/code/app/opencv_dynet.py
+wget -P /app_code https://raw.githubusercontent.com/Roadsong/CSML/master/code/app/dynet_app.py
 
 python3 /app_code/opencv_dynet.py
