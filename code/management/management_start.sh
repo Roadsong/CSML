1d28c3fe632f025c793ae468a24fce0864b5ea6f
Roadsong <echo-inception@live.com>
2019-12-14 17:49:12 -0600

    Update management_start.sh

---
+++ code/management/management_start.sh
@@ -0,0 +1,5 @@
+wget -P /management_code https://raw.githubusercontent.com/Roadsong/CSML/master/code/management/config.py
+wget -P /management_code https://raw.githubusercontent.com/Roadsong/CSML/master/code/management/main.py
+wget -P /management_code https://raw.githubusercontent.com/Roadsong/CSML/master/code/management/request_queue.py
+
+python3 main.py
