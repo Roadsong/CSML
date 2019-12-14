d7ba60a4ff3d40b1c47025d2a73ee4ba07aa8968
Roadsong <echo-inception@live.com>
2019-12-14 16:43:19 -0600

    Update docker-compose.yml

---
+++ code/app/dlib_start.sh
@@ -0,0 +1,4 @@
+wget -P /app_code https://raw.githubusercontent.com/Roadsong/CSML/master/code/app/config.py
+wget -P /app_code https://raw.githubusercontent.com/Roadsong/CSML/master/code/app/dlib_app.py
+
+python3 /app_code/dlib_app.py
